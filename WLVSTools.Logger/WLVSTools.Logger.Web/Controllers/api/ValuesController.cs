5138955592022ddb934fef2579cc228cb964ca9b
Tonton Sevilla <winston_sevilla@outlook.com>
2024-01-28 14:18:14 +0800

    Sample api

---
+++ WLVSTools.Logger/WLVSTools.Logger.Web/Controllers/api/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WLVSTools.Logger.Web.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello");
+        }
+    }
+}
