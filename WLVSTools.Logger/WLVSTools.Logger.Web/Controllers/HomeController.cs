2c0635913ec4ba831f9db1c651ef7ebec1c0bd64
Tonton Sevilla <winston_sevilla@outlook.com>
2024-01-28 14:13:08 +0800

    Initial

---
+++ WLVSTools.Logger/WLVSTools.Logger.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WLVSTools.Logger.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
